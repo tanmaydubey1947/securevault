03f7b635dfe5516788671e37844af404afd2a242
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-09-07 11:07:22 +0530

    Reverted userserivce code from main branch

--- src/main/java/com/securevault/userservice/exception/InvalidUserInformation.java
+++
@@ -1,9 +0,0 @@
-package com.securevault.userservice.exception;
-
-public class InvalidUserInformation extends RuntimeException {
-
-    public InvalidUserInformation(String message) {
-        super(message);
-    }
-
-}
