c1aa8b50e0faac5817d1f9ba8be411c5b3c103e0
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-10-13 12:41:44 +0530

    Created maven project

---
+++ src/main/java/com/securevault/SecureVaultApplication.java
@@ -0,0 +1,13 @@
+package com.securevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureVaultApplication.class, args);
+	}
+
+}
