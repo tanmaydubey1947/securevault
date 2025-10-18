6a5af5968416a5519778a3b17f51181f46546619
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-10-18 19:49:20 +0530

    Added required pom dependencies

--- src/main/java/com/securevault/entity/User.java
+++ src/main/java/com/securevault/entity/User.java
@@ -1,4 +1,16 @@
 package com.securevault.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
 }
