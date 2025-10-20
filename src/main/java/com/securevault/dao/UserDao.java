d24baac44ab30d68044bfa04d6cfec6d7401896a
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-10-20 20:58:41 +0530

    Added new method in user DAO layer to save data

--- src/main/java/com/securevault/dao/UserDao.java
+++ src/main/java/com/securevault/dao/UserDao.java
@@ -18,4 +18,29 @@ public class UserDao {
         return jdbcTemplate.queryForObject(sql, new UsersRowMapper(), email);
     }
 
+    public void saveUser(final User user) {
+        final String sql = """
+                    INSERT INTO users (
+                        full_name,\s
+                        email,\s
+                        password_hash,\s
+                        kyc_status,\s
+                        phone_number,\s
+                        account_status,\s
+                        created_at, \s
+                        role
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        int update = jdbcTemplate.update(sql, user.getFullName(), user.getEmail(), user.getPassword(),
+                user.getKycStatus().name(), user.getPhoneNumber(), user.getAccountStatus().name(),
+                user.getCreatedAt(), user.getRole().name());
+
+        if (update > 0) {
+            log.info("User with email {} saved successfully.", user.getEmail());
+        } else {
+            throw new RuntimeException("Failed to save user with email " + user.getEmail());
+        }
+    }
+
 }
