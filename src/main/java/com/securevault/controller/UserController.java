be5eef8ac0d111b6541ea618c6d68c8d86b6b915
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-10-21 10:59:18 +0530

    Removed unused

--- src/main/java/com/securevault/controller/UserController.java
+++ src/main/java/com/securevault/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.securevault.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
 public class UserController {
+
+
 }
--- src/main/java/com/securevault/repository/PaymentRepository.java
+++
@@ -1,4 +0,0 @@
-package com.securevault.repository;
-
-public interface PaymentRepository {
-}
