347e17222c50a9b2c523e5ccca2d9a894afe6602
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-11-06 21:36:36 +0530

    Added method to do wallet adjustment in send to wallet flow

--- src/main/java/com/securevault/dao/TransactionDao.java
+++ src/main/java/com/securevault/dao/TransactionDao.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+
 import java.sql.PreparedStatement;
 import java.sql.Timestamp;
 import java.time.Instant;
