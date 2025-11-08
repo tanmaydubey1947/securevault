93a39c29d02725e75ed8cf403d525dbf06424d6d
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-11-08 13:13:32 +0530

    Exception handling fix with swagger

--- src/main/java/com/securevault/exception/ApiExceptionHandler.java
+++ src/main/java/com/securevault/exception/ApiExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.securevault.exception;
 
 import com.securevault.exception.payload.ExceptionMessage;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
